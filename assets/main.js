80bb759c6a6e3e2cd7075db60117f2e8f327a21c
Francesco Sica <fsica171@gmail.com>
2021-03-25 17:59:01 +0100

    ...

--- assets/main.js
+++ assets/main.js
@@ -26,23 +26,27 @@ var app = new Vue({
 
     },
 
+    mounted: function () {
+        this.$nextTick(function () {
 
-
-    methods: {
-
-        addTodo: function() {
-            this.a_todo.push({
-                title :  this.newTodo,
-                status : "todo",
-
-            }),
-            this.newTodo = "",
-            console.log(this.a_todo);
-        },
-
-
-
+        })
     },
 
 
+    methods: {
+        addTodo: function() {
+            this.a_todo.unshift({
+
+                title :  this.newTodo.charAt(0).toUpperCase() + this.newTodo.slice(1),
+                status : "todo",
+            }),
+            // Clear input
+            this.newTodo = ""
+        },
+
+        done: function(i) {
+            this.a_todo[i].status = "done";
+            console.log(this.a_todo[i].status);
+        },
+    },
 });
